2b9fbb47e99803fc03e23a93a3c68278aa830b7d
Felipe <felipeeduardofreitasm@gmail.com>
2024-10-19 18:12:53 -0300

    Alteracao nos estilos, finalizacao dos scripts principais e correcao de bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,91 @@
 const blueEssence = document.getElementById('blue-essence-structure');
+let clickPower = document.getElementById('click-power');
+let parsedClickPower = parseInt(clickPower.innerText.slice(12));
 let coinsNumber = document.getElementById('coins-number');
-let clickPower = 1;
 let parsedCoinsNumber = parseInt(coinsNumber.innerText);
 
+let upgradeMinionClickPower = 1;
+let upgradeAcuaminaClickPower = 10;
+let upgradeJinxClickPower = 100;
+let upgradeRyzeClickPower = 1000;
+let upgradeBrandClickPower = 10000;
+let upgradeReiPoro = 100000;
 
-const upgradeMinion = document.getElementById('upgrade-minion')
-let upgradeMinionCount = document.getElementById('upgrade-quantity-counter-minion');
-let parsedUpgradeMinionCount = parseInt(upgradeMinionCount.innerText);
+const audio = new Audio('assets/sounds/pop-sound.mp3');
 
-let upgradeCostMinion = document.getElementById('upgrade-cost-minion')
-let parsedUpgradeCostMinion = parseInt(upgradeCostMinion.innerText)
+blueEssence.addEventListener('click', function () {
 
-
-
-
-
-blueEssence.addEventListener('click', function() {
-
-    parsedCoinsNumber += clickPower;
+    audio.play();
+    
+    parsedCoinsNumber += parsedClickPower;
     coinsNumber.innerText = parsedCoinsNumber;
-
 })
 
-upgradeMinion.addEventListener('click', function() {
-
-    if (parsedCoinsNumber != 0) {
-        //Colocar um let para definir o valor adicionado no click power baseado no poder do minion
-        clickPower++;
-        parsedUpgradeMinionCount++;
-        upgradeMinionCount.innerText = parsedUpgradeMinionCount;
-
-        parsedCoinsNumber -= parsedUpgradeCostMinion;
-
-        coinsNumber.innerText = parsedCoinsNumber;
+class Upgrade {
+ 
+    constructor({ elementID, elementCountID, elementCostID, elementClickPower }) {
+        this.element = document.getElementById(elementID);
+        this.elementCost = document.getElementById(elementCostID);
+        this.parsedElementCost = parseInt(this.elementCost.innerText);
+        this.elementCount = document.getElementById(elementCountID);
+        this.parsedElementCount = parseInt(this.elementCount.innerText);
+        this.elementClickPower = elementClickPower;
+        this.init();
     }
+
+    init() {
+        this.element.addEventListener('click', () => this.upgradeClickPower())
+    }
+
+    upgradeClickPower() {
+
+        if (parsedCoinsNumber >= this.parsedElementCost) {
+
+            parsedClickPower += this.elementClickPower;
+            clickPower.innerText = "Click Power: " + parsedClickPower;
+
+            this.parsedElementCount++;
+            this.elementCount.innerText = this.parsedElementCount;
+
+            parsedCoinsNumber -= this.parsedElementCost;
+
+            coinsNumber.innerText = parsedCoinsNumber;
+        }
+    }
+
+}
+
+let minionUpgrade = new Upgrade({
+    elementID: 'upgrade-minion',
+    elementCostID: 'upgrade-cost-minion',
+    elementCountID: 'upgrade-quantity-counter-minion',
+    elementClickPower: upgradeMinionClickPower,
+});
+
+let acuaminaUpgrade = new Upgrade({
+    elementID: 'upgrade-acuamina',
+    elementCostID: 'upgrade-cost-acuamina',
+    elementCountID: 'upgrade-quantity-counter-acuamina',
+    elementClickPower: upgradeAcuaminaClickPower,
+})
+
+let jinxUpgrade = new Upgrade({
+    elementID: 'upgrade-jinx',
+    elementCostID: 'upgrade-cost-jinx',
+    elementCountID: 'upgrade-quantity-counter-jinx',
+    elementClickPower: upgradeJinxClickPower,
+})
+
+let ryzeUpgrade = new Upgrade({
+    elementID: 'upgrade-ryze',
+    elementCostID: 'upgrade-cost-ryze',
+    elementCountID: 'upgrade-quantity-counter-ryze',
+    elementClickPower: upgradeRyzeClickPower,
+})
+
+let brandUpgrade = new Upgrade({
+    elementID: 'upgrade-brand',
+    elementCostID: 'upgrade-cost-brand',
+    elementCountID: 'upgrade-quantity-counter-brand',
+    elementClickPower: upgradeBrandClickPower,
 })
